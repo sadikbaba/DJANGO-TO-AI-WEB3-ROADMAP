5c4b55a4987ffc9cb50a804b52b2fc8b4d6d9adf
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-03 20:43:53 +0300

    black .

--- assignments/assignment-02-portfolio-site/todo/urls.py
+++ assignments/assignment-02-portfolio-site/todo/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.todo_list, name="todo_list"),
-    path('complete/<int:pk>/', views.complete_todo, name='complete_todo'),
-    path('delete/<int:pk>/', views.delete_todo, name='delete_todo'),
+    path("complete/<int:pk>/", views.complete_todo, name="complete_todo"),
+    path("delete/<int:pk>/", views.delete_todo, name="delete_todo"),
 ]
--- assignments/assignment-02-portfolio-site/todo/views.py
+++ assignments/assignment-02-portfolio-site/todo/views.py
@@ -1,26 +1,23 @@
-from django.shortcuts import render, redirect  
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404, redirect
 from .models import Todo
 from .forms import TodoForm
 
 
-
-
 def todo_list(request):
-  
+
     if request.method == "POST":
         form = TodoForm(request.POST)
         if form.is_valid():
-            form.save() 
-            return redirect("todo_list")  
-            
+            form.save()
+            return redirect("todo_list")
+
     # If it's just a normal page visit (GET request)
     else:
         form = TodoForm()
-        
+
     tasks = Todo.objects.all()
     return render(request, "todo/todo_list.html", {"tasks": tasks, "form": form})
-
 
 
 def complete_todo(request, pk):
@@ -28,9 +25,10 @@ def complete_todo(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     todo.completed = True
     todo.save()  # What method do you call to save this change to the database?
-    return redirect('todo_list')
+    return redirect("todo_list")
+
 
 def delete_todo(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     todo.delete()  # What method do you call to wipe this record from the database?
-    return redirect('todo_list')
+    return redirect("todo_list")
