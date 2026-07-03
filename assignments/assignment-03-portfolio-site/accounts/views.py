76e4642d5ba7e3b89602bb75f401d066f9e34677
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-04 02:50:14 +0300

    finished phase 3 starting phase 4

--- assignments/assignment-03-portfolio-site/accounts/views.py
+++ assignments/assignment-03-portfolio-site/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("dashboard")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "registration/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/register.html", {"form": form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, "registration/dashboard.html")
