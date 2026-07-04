f3890ae2a70882b3bc6fdc8a633b43d8052f5458
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-04 23:13:42 +0300

    working

--- assignments/assignment-04-portfolio-site/e_commerce/models.py
+++ assignments/assignment-04-portfolio-site/e_commerce/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+    def __str__(self):
+        return self.name
--- assignments/assignment-04-portfolio-site/e_commerce/views.py
+++ assignments/assignment-04-portfolio-site/e_commerce/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product
 
 # Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "store/product_list.html"
+    context_object_name = "products"
+    paginate_by = 4
+
+    def get_queryset(self):
+        search = self.request.GET.get("search")
+        if search:
+            return Product.objects.filter(name__icontains=search)
+        return Product.objects.all()
+    
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "store/product_detail.html"
+    context_object_name = "product"
