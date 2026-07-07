e048e715a4e9b712c9da87ed2acedc73d33a55e6
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-07 23:23:08 +0300

    updates seeting env

--- assignments/assignment-06-Deployment/config/settings.py
+++ assignments/assignment-06-Deployment/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l+oiwf+=dv&^(%p#zy@*=enlqz5i&_l*n2^u!n96e5v*4cb)*&"
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
