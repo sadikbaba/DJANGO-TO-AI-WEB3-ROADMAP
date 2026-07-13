741da2498125e66a3097fcb5dbeeafb7c58b8f45
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-13 20:35:25 +0300

    added CurrentUserSerializer

--- assignments/assignment-06-Deployment/blogApi/blogApi/serializers.py
+++ assignments/assignment-06-Deployment/blogApi/blogApi/serializers.py
@@ -39,3 +39,9 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class CurrentUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
