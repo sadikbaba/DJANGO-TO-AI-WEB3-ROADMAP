fd96d48b7e3b03566485f2082beadc50703caa11
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-15 23:11:05 +0300

    template

--- assignments/assignment-07-Real-world/Social-Media-Clone/smclone/urls.py
+++ assignments/assignment-07-Real-world/Social-Media-Clone/smclone/urls.py
@@ -1,2 +1,8 @@
 from .models import Post, Profile
 from django.urls import path
+from .views import profile_detail
+
+urlpatterns = [
+    path("profile/<int:profile_id>/", profile_detail , name="profile_detail"),
+]
+
