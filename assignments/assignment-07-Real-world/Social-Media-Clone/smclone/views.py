63965352996ce5399a6a3c1d098386b71583e70f
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-16 02:17:37 +0300

    template and decuople and

--- assignments/assignment-07-Real-world/Social-Media-Clone/smclone/views.py
+++ assignments/assignment-07-Real-world/Social-Media-Clone/smclone/views.py
@@ -12,3 +12,14 @@ def profile_detail(request, profile_id):
     }
 
     return render(request, "smclone/profile_detail.html", context)
+
+def profile_edit(request, profile_id):
+
+    profile = get_object_or_404(Profile, pk=profile_id)
+
+    context = {
+        "profile" : profile,
+    }
+
+    return render(request, "smclone/profile_edit.html", context)
+
