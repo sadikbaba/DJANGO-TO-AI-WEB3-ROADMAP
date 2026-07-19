603561831f9eb640000fbad6b650bfba0d21070c
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-20 01:39:19 +0300

    comment model

--- assignments/assignment-07-Real-world/Social-Media-Clone/smclone/models.py
+++ assignments/assignment-07-Real-world/Social-Media-Clone/smclone/models.py
@@ -44,3 +44,20 @@ class UsernameRecoveryCode(models.Model):
     code = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField()
+
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+
+    content = models.TextField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return(f"{self.user.username} - {self.post.id}")
+
