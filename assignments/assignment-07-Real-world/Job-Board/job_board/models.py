4a80d5afc38aadb8ce7d57f8ac7b4848a953d1b6
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-21 04:36:50 +0300

    job model

--- assignments/assignment-07-Real-world/Job-Board/job_board/models.py
+++ assignments/assignment-07-Real-world/Job-Board/job_board/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class Company(models.Model):
@@ -13,3 +14,62 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# ====================== CHOICES ======================
+
+
+class ExperienceLevel(models.TextChoices):
+    ENTRY = "entry", "Entry Level"
+    JUNIOR = "junior", "Junior"
+    MID = "mid", "Mid Level"
+    SENIOR = "senior", "Senior"
+
+
+class WorkType(models.TextChoices):
+    REMOTE = "remote", "Remote"
+    ONSITE = "onsite", "On-site"
+    HYBRID = "hybrid", "Hybrid"
+
+
+class EmploymentType(models.TextChoices):
+    FULL_TIME = "full_time", "Full Time"
+    PART_TIME = "part_time", "Part Time"
+    CONTRACT = "contract", "Contract"
+    INTERNSHIP = "internship", "Internship"
+
+
+class JobStatus(models.TextChoices):
+    OPEN = "open", "Open"
+    CLOSED = "closed", "Closed"
+
+
+class Job(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)  
+    title = models.CharField(max_length=200)  
+    description = models.TextField()  
+    requirements = models.TextField()  
+    skills = models.TextField()  
+    location = models.CharField(max_length=200, blank=True)  
+    deadline = models.DateField(null=True, blank=True)  
+    created_at = models.DateTimeField(auto_now_add=True)  
+    salary_negotiable = models.BooleanField(default=False)  
+    salary = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )  
+
+    experience_level = models.CharField(max_length=20, choices=ExperienceLevel.choices)
+
+    work_type = models.CharField(max_length=20, choices=WorkType.choices)
+    employment_type = models.CharField(max_length=20, choices=EmploymentType.choices)
+    status = models.CharField(max_length=20, choices=JobStatus.choices)
+
+    def __str__(self):
+        return self.title
+
+    def clean(self):
+
+        if self.salary is None and self.salary_negotiable is False:
+            raise ValidationError(
+                "Salary must be provided when negotiation is not allowed."
+            )
