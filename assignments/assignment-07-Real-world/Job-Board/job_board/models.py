397113ea4a940356f268ab8460529d17e1eb0c8c
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-21 03:16:05 +0300

    company model

--- assignments/assignment-07-Real-world/Job-Board/job_board/models.py
+++ assignments/assignment-07-Real-world/Job-Board/job_board/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Company(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    description =  models.TextField()
-    logo = models.ImageField(upload_to="company_logos", blank=True)
+    description = models.TextField()
+    logo = models.ImageField(upload_to="company_logos/", blank=True)
     website = models.URLField(blank=True)
     location = models.CharField(max_length=200, blank=True)
-    industry = models.CharField(max_length=200,) 
+    industry = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
-
