307534d1378aa44c981b99a6b6be860db8694482
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-24 21:19:34 +0300

    commited

--- assignments/assignment-07-Real-world/Job-Board/job_board/tests/test_models.py
+++ assignments/assignment-07-Real-world/Job-Board/job_board/tests/test_models.py
@@ -1,21 +1,72 @@
 from django.test import TestCase
-from job_board.models import Company, User
+from job_board.models import (
+    Company,
+    User,
+    Application,
+    Job,
+    ExperienceLevel,
+    WorkType,
+    EmploymentType,
+    JobStatus,
+    ApplicationStatus,
+)
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
 
 
 class CompanyModelTest(TestCase):
-    def test_company_returns_name(self):
-        user = User.objects.create_user(
+
+    def setUp(self):
+        self.user = User.objects.create_user(
             username="sadik", email="sadik@gmail.com", password="12345678"
         )
-        company = Company.objects.create(owner=user, name="Sadik company")
+        self.company = Company.objects.create(owner=self.user, name="Sadik company")
 
-        company_name = str(company)
+    def test_company_returns_name(self):
+        company_name = str(self.company)
         self.assertEqual(company_name, "Sadik company")
 
     def test_company_saved_in_data_base(self):
-        user = User.objects.create_user(
-            username="sadik", email="sadik@gmail.com", password="12345678"
-        )
-        Company.objects.create(owner=user, name="Sadik company")
-
         self.assertEqual(Company.objects.count(), 1)
+
+
+class ApplicationModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="sadik", email="sadik@gmail.com", password="testpassword123"
+        )
+        self.company = Company.objects.create(
+            owner=self.user,
+            name="sadik company",
+        )
+        self.job = Job.objects.create(
+            company=self.company,
+            title="AI writer",
+            description="for writers",
+            requirements="good at AI",
+            skills="most know python",
+            salary_negotiable=False,
+            salary=200,
+            experience_level=ExperienceLevel.ENTRY,
+            work_type=WorkType.REMOTE,
+            employment_type=EmploymentType.FULL_TIME,
+            status=JobStatus.OPEN,
+        )
+
+    def test_user_cannot_apply_twice(self):
+        Application.objects.create(applicant=self.user, job=self.job)
+
+        with self.assertRaises(IntegrityError):
+            Application.objects.create(applicant=self.user, job=self.job)
+
+    def test_cannot_decline_with_out_reason(self):
+        application = Application(
+            applicant=self.user,
+            job=self.job,
+            status=ApplicationStatus.DECLINED,
+            decline_reason="",  # empty reason
+        )
+
+        with self.assertRaises(ValidationError):
+            application.clean()
