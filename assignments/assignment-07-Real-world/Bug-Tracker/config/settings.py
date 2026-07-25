d39b77422fff330160486614f8fca7f8670691da
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-25 23:15:37 +0300

    settings apps

--- assignments/assignment-07-Real-world/Bug-Tracker/config/settings.py
+++ assignments/assignment-07-Real-world/Bug-Tracker/config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.accounts",
+    "apps.projects",
+    "apps.tickets",
+    "apps.comments",
+    "apps.notifications",
 ]
 
 MIDDLEWARE = [
