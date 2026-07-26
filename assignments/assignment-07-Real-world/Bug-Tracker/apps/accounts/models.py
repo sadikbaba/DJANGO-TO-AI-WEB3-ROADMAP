6562808fd295cbd7afbfed826acb45afe8b030bc
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-26 23:47:13 +0300

    profile creation

--- assignments/assignment-07-Real-world/Bug-Tracker/apps/accounts/models.py
+++ assignments/assignment-07-Real-world/Bug-Tracker/apps/accounts/models.py
@@ -4,3 +4,18 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True)
+    website = models.URLField(blank=True)
+    location = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_atm = models.DateTimeField(auto_now=True)
+
+    def full_name(self):
+        return f"{self.user.first_name} {self.user.last_name}"  # i dont know what is .strip() explain to me the i will use it
