f0e0f842bc5e54ea3edfc6412da7cfabe85f3a33
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-28 03:06:18 +0300

    project

--- assignments/assignment-07-Real-world/Bug-Tracker/apps/projects/views.py
+++ assignments/assignment-07-Real-world/Bug-Tracker/apps/projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Project
 
 from .forms import ProjectForm
 
@@ -18,3 +19,78 @@ def project_view(request):
     else:
         form = ProjectForm()
     return render(request, "projects/project.html", {"form": form})
+
+
+@login_required
+def project_list_view(request):
+
+    projects = Project.objects.filter(owner=request.user).order_by("-created_at")
+
+    return render(
+        request,
+        "projects/project_list.html",
+        {
+            "projects": projects,
+        },
+    )
+
+
+@login_required
+def project_detail_view(request, pk):
+
+    project = get_object_or_404(
+        Project,
+        pk=pk,
+        owner=request.user,
+    )
+
+    return render(
+        request,
+        "projects/project_detail.html",
+        {
+            "project": project,
+        },
+    )
+
+@login_required
+def project_edit_view(request, pk):
+
+    project = get_object_or_404(
+        Project,
+        pk=pk,
+        owner=request.user,
+    )
+
+    if request.method == "POST":
+
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect("projects:detail", pk=project.pk)
+    else:
+        form = ProjectForm(instance=project)
+    return render(
+        request, "projects/project_edit.html", {"form": form, "project": project}
+    )
+
+
+@login_required
+def project_delete_view(request, pk):
+
+    project = get_object_or_404(
+        Project,
+        pk=pk,
+        owner=request.user,
+    )
+
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects:list")
+
+    return render(
+        request,
+        "projects/project_delete.html",
+        {
+            "project": project,
+        },
+    )
